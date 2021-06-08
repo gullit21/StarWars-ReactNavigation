8bfc45134c623fd412cdc0179c36a33c8a2002a1
Gullit <gullit.menezes@gmail.com>
2021-06-08 00:03:28 -0300

    commit inicial

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native"
+import {createStackNavigator} from "@react-navigation/stack"
+
+import TelaInicial from './componentes/TelaInicial/';
+import Trilogia1 from './componentes/Trilogia1/';
+import Trilogia2 from './componentes/Trilogia2/';
+import Trilogia3 from './componentes/Trilogia3/';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Tela Inicial" component = {TelaInicial}/>
+        <Stack.Screen name="Trilogia 1" component = {Trilogia1}/>
+        <Stack.Screen name="Trilogia 2" component = {Trilogia2}/>
+        <Stack.Screen name="Trilogia 3" component = {Trilogia3}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
